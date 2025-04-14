7a6ca3fac491ac8df3b90dcdf8dde4d21da3dbe4
pleaobraga <pleao.braga@gmail.com>
2025-04-14 08:01:51 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.+(ts|js)', '**/?(*.)+(spec|test).+(ts|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+}
+
+export default config
