1b7ed961aa792ba8320166e91adfeb2cf03fd924
pleaobraga <pleao.braga@gmail.com>
2025-04-15 06:35:27 -0300

    feat: test create function

--- src/application/product-service.spec.ts
+++ src/application/product-service.spec.ts
@@ -1,4 +1,5 @@
-import { ProductService } from "./product-service"
+import { Product } from './product'
+import { ProductService } from './product-service'
 
 describe('ProductService', () => {
   it('should get a product by id', () => {
@@ -12,5 +13,20 @@ describe('ProductService', () => {
     const product = productService.get('1')
     expect(product).toEqual({ id: '1', name: 'Test Product', price: 100 })
     expect(mockPersistence.get).toHaveBeenCalledWith('1')
+  })
+
+  it('should create a new product', () => {
+    const newProduct = new Product('1', 'Test Product', 100)
+
+    const mockPersistence = {
+      get: jest.fn(),
+      save: jest
+        .fn()
+        .mockReturnValue({ id: '1', name: 'Test Product', price: 100 })
+    }
+    const productService = new ProductService(mockPersistence)
+    const product = productService.create('1', 'Test Product', 100)
+    expect(product).toEqual({ id: '1', name: 'Test Product', price: 100 })
+    expect(mockPersistence.save).toHaveBeenCalledWith(newProduct)
   })
 })
